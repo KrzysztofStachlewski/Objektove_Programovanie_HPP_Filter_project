86090709d707094dac542ac8c10c1ae1e9d3a52b
KrzysztofStachlewski <krystofstachlewski@gmail.com>
2025-05-05 14:15:33 +0200

    Add project files.

---
+++ OP_Stachlewski_Krzysztof_App_Filter/Form1.Designer.cs
@@ -0,0 +1,362 @@
+﻿namespace OP_Stachlewski_Krzysztof_App_Filter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label5 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.label16 = new System.Windows.Forms.Label();
+            this.label17 = new System.Windows.Forms.Label();
+            this.label18 = new System.Windows.Forms.Label();
+            this.label19 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(254)));
+            this.label5.Location = new System.Drawing.Point(9, 76);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(89, 13);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Frekvencia fc:";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.groupBox3);
+            this.groupBox1.Controls.Add(this.groupBox2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Controls.Add(this.label9);
+            this.groupBox1.Controls.Add(this.textBox8);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(362, 324);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.radioButton4);
+            this.groupBox3.Controls.Add(this.radioButton3);
+            this.groupBox3.Location = new System.Drawing.Point(218, 113);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(138, 80);
+            this.groupBox3.TabIndex = 8;
+            this.groupBox3.TabStop = false;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(6, 47);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(63, 17);
+            this.radioButton4.TabIndex = 1;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "C3 ≠ C4";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(7, 23);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(63, 17);
+            this.radioButton3.TabIndex = 0;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "C3 = C4";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.radioButton1);
+            this.groupBox2.Controls.Add(this.radioButton2);
+            this.groupBox2.Location = new System.Drawing.Point(218, 19);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(138, 80);
+            this.groupBox2.TabIndex = 7;
+            this.groupBox2.TabStop = false;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(6, 19);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(63, 17);
+            this.radioButton1.TabIndex = 1;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "C1 = C2";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(6, 42);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(63, 17);
+            this.radioButton2.TabIndex = 2;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "C1 ≠ C2";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(254)));
+            this.label1.Location = new System.Drawing.Point(9, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Typ filtra:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Butterworth",
+            "Bessel",
+            "Čebyšev (Chebyshev)"});
+            this.comboBox1.Location = new System.Drawing.Point(12, 37);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 5;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(254)));
+            this.label9.Location = new System.Drawing.Point(9, 141);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(111, 13);
+            this.label9.TabIndex = 3;
+            this.label9.Text = "Výsledné hodnoty:";
+            // 
+            // textBox8
+            // 
+            this.textBox8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(254)));
+            this.textBox8.Location = new System.Drawing.Point(12, 157);
+            this.textBox8.Multiline = true;
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(173, 161);
+            this.textBox8.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 92);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(121, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 343);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(169, 53);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Vypočítaj";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(380, 23);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(674, 307);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label12
+            // 
+            this.label12.Location = new System.Drawing.Point(556, 52);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(60, 15);
+            this.label12.TabIndex = 5;
+            this.label12.Text = "R1";
+            this.label12.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label13
+            // 
+            this.label13.Location = new System.Drawing.Point(513, 201);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(60, 15);
+            this.label13.TabIndex = 5;
+            this.label13.Text = "            R2";
+            this.label13.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label14
+            // 
+            this.label14.Location = new System.Drawing.Point(814, 51);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(60, 15);
+            this.label14.TabIndex = 5;
+            this.label14.Text = "R3";
+            this.label14.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label15
+            // 
+            this.label15.Location = new System.Drawing.Point(770, 210);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(60, 15);
+            this.label15.TabIndex = 5;
+            this.label15.Text = "            R4";
+            this.label15.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label16
+            // 
+            this.label16.Location = new System.Drawing.Point(513, 125);
+            this.label16.Name = "label16";
+            this.label16.Size = new System.Drawing.Size(60, 15);
+            this.label16.TabIndex = 5;
+            this.label16.Text = "C2";
+            this.label16.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label17
+            // 
+            this.label17.Location = new System.Drawing.Point(776, 131);
+            this.label17.Name = "label17";
+            this.label17.Size = new System.Drawing.Size(60, 15);
+            this.label17.TabIndex = 5;
+            this.label17.Text = " C4   ";
+            this.label17.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label18
+            // 
+            this.label18.Location = new System.Drawing.Point(710, 131);
+            this.label18.Name = "label18";
+            this.label18.Size = new System.Drawing.Size(60, 15);
+            this.label18.TabIndex = 5;
+            this.label18.Text = "      C3";
+            this.label18.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label19
+            // 
+            this.label19.Location = new System.Drawing.Point(444, 125);
+            this.label19.Name = "label19";
+            this.label19.Size = new System.Drawing.Size(60, 15);
+            this.label19.TabIndex = 5;
+            this.label19.Text = "C1";
+            this.label19.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(770, 312);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(275, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Ilustračný obrázok návrhu hornopriepustného filtra 4.rádu";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(1051, 407);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label18);
+            this.Controls.Add(this.label17);
+            this.Controls.Add(this.label15);
+            this.Controls.Add(this.label19);
+            this.Controls.Add(this.label16);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Aplikácia na výpočet hornopriepustného RC filtra 4 rádu";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Label label16;
+        private System.Windows.Forms.Label label17;
+        private System.Windows.Forms.Label label18;
+        private System.Windows.Forms.Label label19;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ OP_Stachlewski_Krzysztof_App_Filter/Form1.cs
@@ -0,0 +1,251 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ToolBar;
+
+namespace OP_Stachlewski_Krzysztof_App_Filter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+        double k = 30000; //Konštanta
+
+        class Butter
+        {
+            public double a1 = 1.8478;
+            public double a2 = 0.7654;
+            public double b1 = 1;
+            public double b2 = 1;
+            public double q1 = 0.54;
+            public double q2 = 1.31;
+
+        }
+
+        class Bessel
+        {
+            public double a1 = 1.3397;
+            public double a2 = 0.7743;
+            public double b1 = 0.4889;
+            public double b2 = 0.3890;
+            public double q1 = 0.52;
+            public double q2 = 0.81;
+
+        }
+
+        class Chebyshev
+        {
+            public double a1 = 2.1853;
+            public double a2 = 0.1964;
+            public double b1 = 5.5339;
+            public double b2 = 1.2009;
+            public double q1 = 1.08;
+            public double q2 = 5.58;
+
+        }
+
+        public double vypocet_c1(double f,double k) 
+        {
+            double  c1 = k / f;
+
+            return c1;
+
+        }
+
+        public double vypocet_c2(double a1, double b1, double c1) 
+        {
+            double c2 = ((4 * b1) / a1) * c1;
+            return c2;
+
+        }
+
+        public double vypocet_c4(double a2, double b2, double c1)
+        {
+            double c4 = ((4 * b2) / a2) * c1;
+            return c4;
+        }
+
+        public double vypocet_r1(double a1, double c1, double f) 
+        {
+            c1 = c1 / Math.Pow(10, 9);
+            double r1 = (1 / (Math.PI * f * c1 * a1));
+            return r1;
+        }
+
+        public double vypocet_r2(double a1, double b1, double c2, double c1, double f)
+        { 
+            if (radioButton1.Checked)
+            {
+                c1 = c1 / Math.Pow(10, 9);
+                double r2 = (a1 / (4 * Math.PI * f * c1 * b1));
+                return r2;
+            }
+            else
+            {
+                c2 = c2 / Math.Pow(10, 9);
+                double r2 = (a1 / (4 * Math.PI * f * c2 * b1));
+                return r2;
+            }
+            
+
+        }
+
+        public double vypocet_r3(double a2, double c1, double f)
+        {
+            c1 = c1 / Math.Pow(10, 9);
+            double r3 = (1 / (Math.PI * f * c1 * a2));
+            return r3;
+        }
+
+        public double vypocet_r4(double a2, double b2, double c4, double c1, double f)
+        {
+            if (radioButton3.Checked)
+            {
+                c1 = c1 / Math.Pow(10, 9);
+                double r4 = (a2 / (4 * Math.PI * f * c1 * b2));
+                return r4;
+            }
+            else
+            {
+                c4 = c4 / Math.Pow(10, 9);
+                double r4 = (a2 / (4 * Math.PI * f * c4 * b2));
+                return r4;
+            }
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            double f;
+            double a1;
+            double b1;
+            double a2;
+            double b2;
+
+            if (double.TryParse(textBox1.Text, out f))
+            {    
+
+                double c1 = vypocet_c1(f,k); //Vypocet C1
+
+                
+                if (comboBox1.SelectedIndex == 0)   //Butterworth
+                {
+                    Butter butter = new Butter();
+                    a1 = butter.a1;
+                    b1 = butter.b1;
+                    a2 = butter.a2;
+                    b2 = butter.b2;
+                }
+                else if (comboBox1.SelectedIndex == 1)  //Bessel
+                {
+                    Bessel bessel = new Bessel();
+                    a1 = bessel.a1;
+                    b1 = bessel.b1;
+                    a2 = bessel.a2;
+                    b2 = bessel.b2;
+                }
+                else if (comboBox1.SelectedIndex == 2)  //Čebyšev
+                {
+                    Chebyshev chebyshev = new Chebyshev();
+                    a1 = chebyshev.a1;
+                    b1 = chebyshev.b1;
+                    a2 = chebyshev.a2;
+                    b2 = chebyshev.b2;
+                }
+                else
+                {
+                    MessageBox.Show("Zabudli ste vybrať filter!");
+                    return;
+                }
+
+                if ((radioButton1.Checked || radioButton2.Checked) && (radioButton3.Checked || radioButton4.Checked))
+                {
+                    double c2 = vypocet_c2(a1, b1, c1);         //Vypocet C2
+                    double c4 = vypocet_c4(a2, b2, c1);         //Vypocet C3
+
+                    double r1 = vypocet_r1(a1, c1, f);          //vypocet R1
+                    double r2 = vypocet_r2(a1, b1, c2, c1, f);  //vypocet R2
+                    double r3 = vypocet_r3(a2, c1, f);          //Vypocet R3
+                    double r4 = vypocet_r4(a2, b2, c4, c1, f);  //Vypocet R4
+
+                    DialogResult prepocet = MessageBox.Show("Chcete výsledné hodnoty prepočítať na KΩ?", "Prepočet", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                    if (prepocet == DialogResult.Yes) 
+                    {
+                        double[] hodnoty = { r1, r2, r3, r4 };
+                        for (int i = 0; i < hodnoty.Length; i++)
+                        {
+                            hodnoty[i] = hodnoty[i] / 1000;
+                            (r1, r2, r3, r4) = (hodnoty[0], hodnoty[1], hodnoty[2], hodnoty[3]);
+                        }
+
+                    textBox8.Text = $"C1 = {c1:F2} nF{Environment.NewLine}" +
+                                    $"C2 = {c2:F2} nF{Environment.NewLine}" +
+                                    $"C3 = {c1:F2} nF{Environment.NewLine}" +
+                                    $"C4 = {c4:F2} nF{Environment.NewLine}" +
+                                    $"_______________{Environment.NewLine}" +
+                                    $"R1 = {r1:F2} kΩ {Environment.NewLine}" +
+                                    $"R2 = {r2:F2} kΩ {Environment.NewLine}" +
+                                    $"R3 = {r3:F2} kΩ {Environment.NewLine}" +
+                                    $"R4 = {r4:F2} kΩ {Environment.NewLine}" ;
+
+                    label19.Text = $"{c1:F2} nF";
+                    label16.Text = $"{c2:F2} nF";
+                    label12.Text = $"{r1:F2} kΩ";
+                    label13.Text = $"{r2:F2} kΩ";
+                    label18.Text = $"{c1:F2} nF";
+                    label17.Text = $"{c4:F2} nF";
+                    label14.Text = $"{r3:F2} kΩ";
+                    label15.Text = $"{r4:F2} kΩ";
+                    }
+                    else if (prepocet == DialogResult.No)
+                    {
+                    textBox8.Text = $"C1 = {c1:F2} nF{Environment.NewLine}" +
+                                    $"C2 = {c2:F2} nF{Environment.NewLine}" +
+                                    $"C3 = {c1:F2} nF{Environment.NewLine}" +
+                                    $"C4 = {c4:F2} nF{Environment.NewLine}" +
+                                    $"_______________{Environment.NewLine}" +
+                                    $"R1 = {r1:F2} Ω {Environment.NewLine}" +
+                                    $"R2 = {r2:F2} Ω {Environment.NewLine}" +
+                                    $"R3 = {r3:F2} Ω {Environment.NewLine}" +
+                                    $"R4 = {r4:F2} Ω {Environment.NewLine}";
+
+                    label19.Text = $"{c1:F2} nF";
+                    label16.Text = $"{c2:F2} nF";
+                    label12.Text = $"{r1:F2} Ω";
+                    label13.Text = $"{r2:F2} Ω";
+                    label18.Text = $"{c1:F2} nF";
+                    label17.Text = $"{c4:F2} nF";
+                    label14.Text = $"{r3:F2} Ω";
+                    label15.Text = $"{r4:F2} Ω";
+
+                    }
+
+
+                }
+                else
+                {
+                    MessageBox.Show("Vyberte či sa majú kondenzátory rovnať!");
+                }
+            }   
+            else
+            {
+                MessageBox.Show($"Pozor zabudli ste zadať hodnotu frekvencie!");
+            }            
+
+        }
+
+    }
+}
